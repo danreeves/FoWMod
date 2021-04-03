59151f0b03a80aa261a14f883eedd18258729c96
Dan Reeves <hey@danreev.es>
2021-04-03 21:57:09 +0100

    delete bundle folders

--- scripts/mods/FoWMod/FoWMod.lua
+++ scripts/mods/FoWMod/FoWMod.lua
@@ -4,7 +4,7 @@ local mod = get_mod("FoWMod")
 mod:dofile("scripts/mods/FoWMod/debug_spawners")
 local custom_terror_events = mod:dofile("scripts/mods/FoWMod/terror_events_plaza")
 
-local default_terror_events = TerrorEventBlueprints.plaza
+fow_default_terror_events = fow_default_terror_events or table.clone(TerrorEventBlueprints.plaza)
 
 if mod:is_enabled() then
   TerrorEventBlueprints.plaza = custom_terror_events
@@ -15,6 +15,15 @@ function mod.on_enabled()
 end
 
 function mod.on_disabled()
-  TerrorEventBlueprints.plaza = default_terror_events
+  TerrorEventBlueprints.plaza = fow_default_terror_events
 end
 
+mod:command("debug_toggle_plaza", "", function()
+  if TerrorEventBlueprints.plaza == fow_default_terror_events then
+	mod:echo("setting custom terror events")
+	TerrorEventBlueprints.plaza = custom_terror_events
+  else
+	mod:echo("setting default terror events")
+	TerrorEventBlueprints.plaza = fow_default_terror_events
+  end
+end)
